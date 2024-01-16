8264f2f3dcded326de44334328f04b81b4821c25
Ayush Agarwal <115417597+Ayush1234871@users.noreply.github.com>
2024-01-16 19:18:25 +0530

    Add files via upload

---
+++ To do list/script.js
@@ -0,0 +1,45 @@
+(() => {
+    let toDoListArray = [];
+    const form = document.querySelector(".form");
+    const input = form.querySelector(".form__input");
+    const ul = document.querySelector(".toDoList");
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      let itemId = String(Date.now());
+      let toDoItem = input.value;
+      addItemToDOM(itemId , toDoItem);
+      addItemToArray(itemId, toDoItem);
+      input.value = '';
+    });
+  
+    ul.addEventListener('click', e => {
+      let id = e.target.getAttribute('data-id')
+      if (!id) return 
+      removeItemFromDOM(id);
+      removeItemFromArray(id);
+    });
+  
+    function addItemToDOM(itemId, toDoItem) {
+      const li = document.createElement('li')
+      li.setAttribute("data-id", itemId);
+      li.innerText = toDoItem
+      ul.appendChild(li);
+    }
+  
+    function addItemToArray(itemId, toDoItem) {
+      toDoListArray.push({ itemId, toDoItem});
+      console.log(toDoListArray)
+    }
+  
+    function removeItemFromDOM(id) {
+      var li = document.querySelector('[data-id="' + id + '"]');
+      ul.removeChild(li);
+    }
+  
+    function removeItemFromArray(id) {
+      toDoListArray = toDoListArray.filter(item => item.itemId !== id);
+      console.log(toDoListArray);
+    }
+  
+  })();
